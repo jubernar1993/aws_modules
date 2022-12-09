69e5ee9691f35c4a62fd8dc9fb9ec6bdb155d927
July Bernard <julybernard1993@gmail.com>
2022-12-09 17:23:26 +0000

    deleted the security group

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "terraformjubernartesting"
+    key    = "terraformjubernartesting/dev"
+    region = "us-east-1"
+  }
+}
+
+
+
+
+
 provider "aws"{
 profile = var.profile 
 region = var.region
