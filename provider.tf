ad585268b0e942a7060b271de0d4a7e0bc959049
July Bernard <julybernard1993@gmail.com>
2022-12-13 16:59:49 +0000

    backend configuration for s3 bucket to store .tfstate file works v1

--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
-
+terraform {
+  backend "s3" {
+    bucket = "my-tf-t1234est-bucket"
+    key    = "my-tf-t1234est-bucket/dev"
+    region = "us-east-1"
+  }
+}
 provider "aws"{
 profile = var.profile 
 region = var.region
